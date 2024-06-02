d47b3bb10ac5a5522eb9acf31d03f2ddd0342853
roksana-barna <roksanabarna1803@gmail.com>
2024-06-02 11:37:40 +0600

    increment

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment,decrement } from '../redux/features/counter/counterSlice';
+import { increment,decrement,incrementByValue} from '../redux/features/counter/counterSlice';
 
 const Counter = () => {
     const count = useSelector ((state)=>state.counter.count)
@@ -8,7 +8,9 @@ const Counter = () => {
     console.log(count)
     return (
         <div>
+            <button onClick={()=>dispatch (incrementByValue(5))}>increment by 5 </button><br/>
             <button onClick={()=>dispatch (increment())}>increment</button>
+
             <div>
                 <h1>{count}</h1>
             </div>
